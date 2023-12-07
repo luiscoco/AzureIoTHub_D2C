3d3aa79a5160661ab4d9c3ca5ee93838d00545ac
Luis Coco Enríquez <LEnriquez@luxoft.com>
2023-12-07 17:19:36 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Azure.Devices.Client;
+using System.Text;
+
+
+string ConnectionString = "HostName=myIoTHubname1974.azure-devices.net;DeviceId=myDevice;SharedAccessKey=EzLbRGucSovGeSzk8WcfIvDuTqk752tpRAIoTO9Zbfk=";
+
+var message = "Hello from Azure IoT Hub!";
+await SendMessageToIoTHubAsync(message);
+
+async Task SendMessageToIoTHubAsync(string message)
+{
+    var deviceClient = DeviceClient.CreateFromConnectionString(ConnectionString, TransportType.Mqtt);
+
+    var payload = new Message(Encoding.UTF8.GetBytes(message));
+
+    try
+    {
+        await deviceClient.SendEventAsync(payload);
+        Console.WriteLine($"Message sent: {message}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error sending message: {ex.Message}");
+    }
+    finally
+    {
+        await deviceClient.CloseAsync();
+    }
+}
